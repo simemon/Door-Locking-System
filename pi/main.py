a63c4d91e3b1de6f935fb00d0693e0a00dee21ab
Siraj Memon <siraj1@umbc.edu>
2016-05-06 21:20:46 -0400

    Adding python based source code to control raspberry pi and capture image and handle matrix keypad

---
+++ pi/main.py
@@ -0,0 +1,183 @@
+#########################
+'''
+1. Take EXPECTED PIN
+2. take image and store it into img folder of wamp with filename as timestamp
+3. store the entry into database with image location
+4. run a PHP script and send notification whether it was the right pin or not.
+5. change the led accordingly
+'''
+
+#########################
+
+#echo mmc0 > /sys/class/leds/led0/trigger		#To reset LED to its default scenario
+#echo 1 > /sys/class/leds/led0/brightness		#To set LED (programmable)
+#echo 0 > /sys/class/leds/led0/brightness		#To reset LED (programmable)
+
+import RPi.GPIO as GPIO
+import time
+import cv2
+from datetime import date, datetime, timedelta
+import mysql.connector
+import sys
+import os
+
+#database Related Variables
+
+HOST = '127.0.0.1'
+USERNAME = 'root'
+DATABASE = 'ulock'
+PASSWORD = 'root'
+
+#Global Part
+
+DEBUG = True
+
+#Step 1
+
+#GPIO Related Stuff
+GPIO.setmode(GPIO.BOARD)
+MATRIX = [ [1,2,3,'A'],
+	   [4,5,6,'B'],
+	   [7,8,9,'C'],
+	   ['*',0,'#','D']  ]
+ROW = [7,11,13,15]
+COL = [12,16,18,22]
+EXPECTED_PIN = [1,2,3,4]
+
+FILE_PATH = '/var/www/html/GCM/img/'
+
+#Init
+
+def init():
+	#GPIO.setwarnings(False)
+	for j in range(4):
+		GPIO.setup(COL[j], GPIO.OUT)
+		GPIO.output(COL[j], 1)
+	for i in range(4):
+		GPIO.setup(ROW[i], GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+	os.system("echo 0 > /sys/class/leds/led0/brightness")		#To reset LED (programmable)
+
+key = []
+def handleGPIO():
+	global key
+	key = []
+	try:
+		while(True):
+			for j in range(4):
+				GPIO.output(COL[j],0)
+				for i  in range(4):
+					if GPIO.input(ROW[i]) == 0:
+						print MATRIX[i][j]
+						if(MATRIX[i][j] == 'D'):
+							return		
+						if(MATRIX[i][j] != '*'):		
+							key.append(MATRIX[i][j])
+						time.sleep(0.3)
+						while(GPIO.input(ROW[i]) == 0):
+							pass
+				GPIO.output(COL[j],1)
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+
+# Captures a single image from the camera and returns it in PIL format
+def get_image(camera):
+	 # read is the easiest way to get a full image out of a VideoCapture object.
+	 retval, im = camera.read()
+	 return im
+
+def capture_image(IMAGE_NAME):
+	# Camera 0 is the integrated web cam on my netbook
+	camera_port = 0
+	 
+	#Number of frames to throw away while the camera adjusts to light levels
+	ramp_frames = 30
+	 
+	# Now we can initialize the camera capture object with the cv2.VideoCapture class.
+	# All it needs is the index to a camera port.
+	camera = cv2.VideoCapture(camera_port)
+
+	# Ramp the camera - these frames will be discarded and are only used to allow v4l2
+	# to adjust light levels, if necessary
+	for i in xrange(ramp_frames):
+		temp = get_image(camera)
+	print("Taking image...")
+	# Take the actual image we want to keep
+	camera_capture = get_image(camera)
+	file = FILE_PATH + IMAGE_NAME
+	# A nice feature of the imwrite method is that it will automatically choose the
+	# correct format based on the file extension you provide. Convenient!
+	cv2.imwrite(file, camera_capture)
+	 
+	# You'll want to release the camera, otherwise you won't be able to create a new
+	# capture object until your script exits
+	del(camera)
+
+
+def LED(success):
+	if success:
+		os.system("echo 1 > /sys/class/leds/led0/brightness")		#To set LED (programmable)
+	else:
+		os.system("echo 0 > /sys/class/leds/led0/brightness")		#To reset LED (programmable)		
+
+
+def update_db_status(SET):
+	db = mysql.connector.connect(host=HOST,user=USERNAME,password=PASSWORD,database=DATABASE)
+	update_str = "update door_status set value = %d where id = 1" % (SET)
+	if DEBUG:
+		print update_str
+	cursor = db.cursor()
+	cursor.execute(update_str)
+	db.commit()
+	cursor.close()
+	db.close()
+
+def main():
+	global key
+	print 'In main'
+	init()
+	while True:	
+		success = False	
+		counter = 3	
+		for i in range(counter):
+			handleGPIO()
+			if DEBUG:
+				for k in key:
+					print k,
+				print			
+			if(key == EXPECTED_PIN):
+				print 'SUCCESS'
+				success = True
+				break
+			else:
+				print 'FAILED', i
+		
+		LED(success)		
+
+		update_db_status(success)
+
+		TIMESTAMP = datetime.now()
+		IMAGE_NAME = TIMESTAMP.strftime('%Y-%d-%m-%H-%M-%S')
+
+		if success:
+			IMAGE_NAME += '_auth'
+		else:
+			IMAGE_NAME += '_deauth'
+
+		IMAGE_NAME += ".png"
+			
+
+		capture_image(IMAGE_NAME)
+		db = mysql.connector.connect(host=HOST,user=USERNAME,password=PASSWORD,database=DATABASE)
+		insert_str = "insert into entry (imageloc,timestamp,valid,seen) values ('%s', '%s', %d, %d)" % (IMAGE_NAME , TIMESTAMP, success, 0)
+		cursor = db.cursor()
+		cursor.execute(insert_str)
+
+		if DEBUG:
+			print insert_str
+			print("Committed")
+		db.commit()
+		cursor.close()
+		db.close()
+
+main()
